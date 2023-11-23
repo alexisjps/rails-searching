5755bec0a0c510151fe2d02f7c5b599431fb156a
alexis stephan <alexis-stephan@hotmail.fr>
2023-11-23 09:59:39 +0100

    first part
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,13 @@
+class MoviesController < ApplicationController
+  # def index
+  #   # @movies = Movie.all
+  #   if params[:query].present?
+  #     @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+  #   else
+  #     @movies = Movie.all
+  #   end
+  # end
+
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_081556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "directors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.string "synopsis"
+    t.bigint "director_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+  end
+
+  add_foreign_key "movies", "directors"
 end
